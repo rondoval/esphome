cb3af16f2a7a2aecf2cd286cdf2b850518d4b324
Łukasz Winczura <rondoval@gmail.com>
2019-05-12 17:58:43 +0200

    Floating point version of calc_heater_resistance_ This is consistent with formulas used in other methods.

--- esphome/components/bme680/bme680.cpp
+++ esphome/components/bme680/bme680.cpp
@@ -243,33 +243,30 @@ void BME680Component::update() {
 }
 
 uint8_t BME680Component::calc_heater_resistance_(uint16_t temperature) {
-  if (temperature < 200)
-    temperature = 200;
   if (temperature > 400)
     temperature = 400;
 
-  const uint8_t ambient_temperature = this->calibration_.ambient_temperature;
-  const int8_t gh1 = this->calibration_.gh1;
-  const int16_t gh2 = this->calibration_.gh2;
-  const int8_t gh3 = this->calibration_.gh3;
-  const uint8_t res_heat_range = this->calibration_.res_heat_range;
-  const uint8_t res_heat_val = this->calibration_.res_heat_val;
+  const float ambient_temperature = this->calibration_.ambient_temperature;
+  const float gh1 = this->calibration_.gh1;
+  const float gh2 = this->calibration_.gh2;
+  const float gh3 = this->calibration_.gh3;
+  const float res_heat_range = this->calibration_.res_heat_range;
+  const float res_heat_val = this->calibration_.res_heat_val;
 
-  uint8_t heatr_res;
-  int32_t var1;
-  int32_t var2;
-  int32_t var3;
-  int32_t var4;
-  int32_t var5;
-  int32_t heatr_res_x100;
+  float var1 = 0;
+  float var2 = 0;
+  float var3 = 0;
+  float var4 = 0;
+  float var5 = 0;
+  uint8_t heatr_res = 0;
 
-  var1 = (((int32_t) ambient_temperature * gh3) / 1000) * 256;
-  var2 = (gh1 + 784) * (((((gh2 + 154009) * temperature * 5) / 100) + 3276800) / 10);
-  var3 = var1 + (var2 / 2);
-  var4 = (var3 / (res_heat_range + 4));
-  var5 = (131 * res_heat_val) + 65536;
-  heatr_res_x100 = (int32_t)(((var4 / var5) - 250) * 34);
-  heatr_res = (uint8_t)((heatr_res_x100 + 50) / 100);
+  var1 = (gh1 / 16.0f) + 49.0f;
+  var2 = ((gh2 / 32768.0f) * 0.0005f) + 0.00235f;
+  var3 = gh3 / 1024.0f;
+  var4 = var1 * (1.0f + (var2 * (float)temperature));
+  var5 = var4 + (var3 * ambient_temperature);
+  heatr_res = (uint8_t)(3.4f * ((var5 * (4 / (4 + res_heat_range)) *
+    (1/(1 + (calib.res_heat_val * 0.002f)))) - 25));
 
   return heatr_res;
 }
